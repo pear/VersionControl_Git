54d96923bfad661cdcecedb20ca711d71072672b
Kousuke Ebihara <ebihara@tejimaya.com>
2010-01-20 21:32:17 +0900

    added test/AllTest.php

---
+++ test/AllTest.php
@@ -0,0 +1,24 @@
+<?php
+
+chdir(dirname(__FILE__));
+set_include_path(get_include_path().PATH_SEPARATOR.realpath('../../'));
+
+require_once 'PHPUnit/Framework.php';
+
+require_once './VersionControl_GitTest.php';
+require_once './VersionControl_Git_ComponentTest.php';
+require_once './VersionControl_Git_ObjectTest.php';
+
+class AllTest
+{
+  public static function suite()
+  {
+    $suite = new PHPUnit_Framework_TestSuite('');
+
+    $suite->addTestSuite('VersionControl_GitTest');
+    $suite->addTestSuite('VersionControl_Git_ComponentTest');
+    $suite->addTestSuite('VersionControl_Git_ObjectTest');
+
+    return $suite;
+  }
+}
